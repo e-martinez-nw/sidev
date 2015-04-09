ca56f8028abd6a211bcb0b34627441d20a554270
Eduardo <e.martinez@nuevaweb.com.mx>
2015-04-09 13:15:38 -0500

    [Header]Se agregó menú dropdown [CSS] Se hicieron ajustes para dispositivos móviles

--- header.php
+++ header.php
@@ -32,31 +32,28 @@
 
 </head>
 <body>
-
-	<?php 
-	/*
-	*
-	* El css que genera el "En Construcción" está salvado en el archivo .less
-	* únicamente comenta la línea o elimínala para quitar el display en construcción
-	*
-	* Elimina éste comentario y ya estás listo 
-	* Happy Developing!
-	*
-	*/
-	?>
-
 	<header id="main-header">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-2">
+				<div class="col-sm-12">
 					<a href="<?php bloginfo('url'); ?>" class="logo">
 						<img src="<?php bloginfo('template_url'); ?>/img/index/sidev.png" />
 					</a>
-				</div><!-- col-sm-2 -->
-				<div class="col-sm-10">
-					<?php nw_main_nav(); ?>
-					<!-- <div class="dropdown_bg"></div> -->
+					<div class="navbar-header">
+				    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+				      		<span class="sr-only">Toggle navigation</span>
+				        	<span class="icon-bar"></span>
+				        	<span class="icon-bar"></span>
+				        	<span class="icon-bar"></span>
+				    	</button>
+						<a class="navbar-brand" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
+				      		<h1 class="none"><?php bloginfo('name'); ?></h1>
+						</a>
+			    	</div>
+					<div class="dropdown_bg">
+						<?php nw_main_nav(); ?>
+					</div>
 				</div><!-- col-sm-10 -->
-			</div>
-		</div>
+			</div><!--row-->
+		</div><!--container-->
 	</header><!-- /#main-header -->
